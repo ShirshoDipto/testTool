7757e8532e0c12c1df624c860269165bf556b964
ShirshoDipto <shirshodipto@gmail.com>
2023-12-24 17:16:42 +0600

    Edited the greeting function

--- greetings_tool.py
+++ greetings_tool.py
@@ -15,11 +15,22 @@ class GreetingsTool(BaseTool):
     args_schema: Type[BaseModel] = GreetingsInput
     description: str = "Sends a Greeting Message"
 
-    def _execute(self, greetings: str = None):
+
+    def _execute(self, greetings: dict = None):
         from_name = self.get_tool_config('FROM')
-        if type(greetings) != str:
-            return "Hello" + "\n" + from_name
-        else:
-            greetings_str = greetings + "\n" + from_name
-            return greetings_str
+    
+        if not isinstance(greetings, dict):
+            return f"Hello There!!\n{from_name}"
+
+        greetings_message = greetings.get("greetings", "")
+        composed_message = f"{greetings_message}\n{from_name}"
+        return composed_message
+
+    # def _execute(self, greetings: dict = None):
+    #     from_name = self.get_tool_config('FROM')
+    #     if type(greetings) != dict:
+    #         return "Hello" + "\n" + from_name
+    #     else:
+    #         greetings_str = greetings["greetings"] + "\n" + from_name
+    #         return greetings_str
                 
